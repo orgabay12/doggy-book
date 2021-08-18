ed3ea1bb27bce9fd0e78500b2e446ae9bd6a3dd7
or <orgabay12@gmail.com>
2021-08-18 13:17:38 +0300

    Add models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import VaccineType, Dog, Vaccine
+
+
+@admin.register(VaccineType)
+class VaccineTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'months_interval')
+
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'breed')
+
+
+@admin.register(Vaccine)
+class VaccineAdmin(admin.ModelAdmin):
+    list_display = ('type', 'dog', 'date')
--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class VaccineType(models.Model):
+    name = models.CharField(max_length=50)
+    months_interval = models.IntegerField(default=3)
+
+    def __str__(self):
+        return self.name
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    owner = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+
+class Vaccine(models.Model):
+    type = models.ForeignKey(VaccineType, on_delete=models.CASCADE)
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    date = models.DateField()
+    vet_name = models.CharField(max_length=50, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    schedule_next = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.type.name}-{self.date.month}"
