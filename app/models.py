36a04d8ae220ba63c10fdc4e1f7b46ac8c92fcb3
or <orgabay12@gmail.com>
2021-08-24 20:35:12 +0300

    fix

--- app/models.py
+++ app/models.py
@@ -25,9 +25,9 @@ class Vaccine(models.Model):
     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
     time = models.DateTimeField()
     vet_name = models.CharField(max_length=50, blank=True, null=True)
-    notes = models.TextField(blank=True, null=True)
     schedule_next = models.BooleanField(default=False)
     status = models.CharField(max_length=20, choices=(("Todo", "Todo"), ("Done", "Done")), default="Todo")
+    notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.type.name}-{self.time.strftime('%B')}"
