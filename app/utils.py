ba5d03e48c6c176d3cf44c7d078c141e2300ec1a
or <orgabay12@gmail.com>
2021-08-24 20:17:32 +0300

    implement ics logic

--- app/utils.py
+++ app/utils.py
@@ -62,7 +62,7 @@ def send_event(login_address, password, attendees, sender_name, subject, start_d
     dtstart = start_datetime.strftime("%Y%m%dT%H%M%SZ")
     dtend = dtend.strftime("%Y%m%dT%H%M%SZ")
 
-    description = "DESCRIPTION: test invitation from pyICSParser" + CRLF
+    description = "DESCRIPTION: vaccine invitation from doogy book" + CRLF
     attendee = ""
     for att in attendees:
         attendee += "ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-    PARTICIPANT;PARTSTAT=ACCEPTED;RSVP=TRUE" + CRLF + " ;CN=" + att + ";X-NUM-GUESTS=0:" + CRLF + " mailto:" + att + CRLF
@@ -70,8 +70,7 @@ def send_event(login_address, password, attendees, sender_name, subject, start_d
     ical += "METHOD:REQUEST" + CRLF + "BEGIN:VEVENT" + CRLF + "DTSTART:" + dtstart + CRLF + "DTEND:" + dtend + CRLF + "DTSTAMP:" + dtstamp + CRLF + organizer + CRLF
     ical += "UID:FIXMEUID" + dtstamp + CRLF
     ical += attendee + "CREATED:" + dtstamp + CRLF + description + "LAST-MODIFIED:" + dtstamp + CRLF + "LOCATION:" + CRLF + "SEQUENCE:0" + CRLF + "STATUS:CONFIRMED" + CRLF
-    ical += "SUMMARY:test " + start_datetime.strftime(
-        "%Y%m%d @ %H:%M") + CRLF + "TRANSP:OPAQUE" + CRLF + "END:VEVENT" + CRLF + "END:VCALENDAR" + CRLF
+    ical += "SUMMARY:" + subject + CRLF + "TRANSP:OPAQUE" + CRLF + "END:VEVENT" + CRLF + "END:VCALENDAR" + CRLF
 
     msg = MIMEMultipart('mixed')
     msg['Reply-To'] = sender
