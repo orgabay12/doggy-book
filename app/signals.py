fc5bfe59f7e58674eea4e3b55e29d7859cc47df9
or <orgabay12@gmail.com>
2021-08-24 20:34:36 +0300

    fix

--- app/signals.py
+++ app/signals.py
@@ -11,8 +11,8 @@ from app.utils import send_event
 def schedule_next_vaccine(sender, instance, created, **kwargs):
     if instance.status == "Done" and instance.schedule_next:
         interval = instance.type.months_interval
-        next_vaccine_date = instance.date + relativedelta(months=interval)
-        Vaccine.objects.get_or_create(type=instance.type, dog=instance.dog, date=next_vaccine_date,
+        next_vaccine_date = instance.time + relativedelta(months=interval)
+        Vaccine.objects.get_or_create(type=instance.type, dog=instance.dog, time=next_vaccine_date,
                                       vet_name=instance.vet_name, schedule_next=True)
 
 
